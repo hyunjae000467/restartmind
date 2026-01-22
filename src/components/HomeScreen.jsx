f632ce3c40c89531768ac5ce9ae3f990074d7f98
OrangeZone <hyunjae000467@gmail.com>
2026-01-22 21:26:58 +0900

    design change 1st

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import bgImage from '../assets/home_bg_geometric.png';
 
 const HomeScreen = ({ onStartTest, onViewColumns }) => {
     return (
-        <div className="fade-in" style={{ textAlign: 'left', padding: '20px 0' }}>
+        <div className="fade-in" style={{
+            textAlign: 'left',
+            padding: '20px 0',
+            minHeight: '80vh',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            position: 'relative'
+        }}>
+            {/* Background Image Layer */}
+            <div style={{
+                position: 'fixed',
+                top: 0,
+                right: 0,
+                bottom: 0,
+                left: 0,
+                zIndex: -1,
+                backgroundImage: `url(${bgImage})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'right bottom',
+                backgroundSize: 'contain', // Or 'cover' depending on preference, 'contain' fits the corner better usually
+                opacity: 0.8
+            }} />
+
             <h1 className="title" style={{ fontSize: '3rem', marginBottom: '10px' }}>RE:START</h1>
             <p style={{ fontSize: '1.2rem', color: 'var(--text-secondary)', marginBottom: '60px' }}>
                 슬럼프를 극복하고 다시 시작하는 힘
